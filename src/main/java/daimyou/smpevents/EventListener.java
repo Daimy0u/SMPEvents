505860a169ebc07ca2deadad272c5aee0bda4bf8
Yusuke Matsuki <yusukematsuki123@gmail.com>
2021-03-26 10:06:58 +0700

    UPDATE #0002

--- src/main/java/daimyou/smpevents/EventListener.java
+++ src/main/java/daimyou/smpevents/EventListener.java
@@ -4,9 +4,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import daimyou.smpevents.eventTracker;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.entity.*;
 
-public class EventListener extends eventTracker implements Listener {
+public class EventListener implements Listener {
     eventTracker eventTrack = new eventTracker();
     @EventHandler
     public void onDeath(EntityDeathEvent e){
@@ -17,5 +18,10 @@ public class EventListener extends eventTracker implements Listener {
             Player player = e.getEntity().getKiller();
             eventTrack.updatePlayerCount(player.getName(), "MOBCOUNT");
         }
+    }
+
+    @EventHandler
+    public void onKill(PlayerDeathEvent e) {
+        
     }
 }
